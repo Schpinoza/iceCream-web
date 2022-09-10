35e69d775a2ef565dc85f897395db9e5980a2807
Schpinoza <tal_gab@hotmail.com>
2022-09-10 12:39:12 +0300

    folder name change

--- client/src/App.js
+++ client/src/App.js
@@ -2,14 +2,14 @@ import React from "react";
 import { useSelector } from "react-redux";
 import Navbar from "./components/Navbar/Navbar";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Home from "./components/pages/Home";
+import Home from "./components/Pages/Home";
 import IceCream from "./components/IceCream/IceCream";
 import Footer from "./components/UI/Footer";
 import Cart from "./components/Cart/Cart";
-import Shipping from "./components/shipping/Shipping";
+import Shipping from "./components/Shipping/Shipping";
 import OrderList from "./components/OrderList/OrderList";
 import GlobalStyles from "./Globalstyles";
-import ConactUs from "./components/pages/ConactUs";
+import ConactUs from "./components/Pages/ConactUs";
 
 const App = () => {
   const modalOn = useSelector((state) => {
