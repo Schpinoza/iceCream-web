1f0d729fd3b1bf6df0ff35cf373341e10195f938
Schpinoza <tal_gab@hotmail.com>
2022-09-11 12:19:34 +0300

    change folder

--- server/services/db.js
+++ server/services/db.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const { config } = require("../../config");
+const { config } = require("../config");
 
 const MONGODB_URL = config.MONGODB_KEY_URL;
 const init = async () => {
