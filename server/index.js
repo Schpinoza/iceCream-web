d868bea35d11af6c94fc7ae645ea6fdb3f5f3b01
Schpinoza <tal_gab@hotmail.com>
2022-09-11 16:28:40 +0300

    upadeed json

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ require("dotenv").config();
 const PORT = process.env.PORT || 3001;
 
 const app = express();
-app.use(express.json()); // for parsing application/json
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 init();
