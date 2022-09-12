855d6d1c8be7fd2b4d6311cdb75dbdff1f2d37da
Schpinoza <tal_gab@hotmail.com>
2022-09-12 10:47:06 +0300

    config beed deleted

--- server/services/db.js
+++ server/services/db.js
@@ -1,6 +1,4 @@
 const mongoose = require("mongoose");
-const { config } = require("../config");
-
 const init = async () => {
   mongoose.connect(process.env.MONGODB_URL, { useNewUrlParser: true });
 };
